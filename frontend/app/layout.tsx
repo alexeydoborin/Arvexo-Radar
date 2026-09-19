7a7c07e92f7eb4ab13163e73df50775d4128d7b4
Alexey <alexeydoborin@gmail.com>
2026-09-20 00:17:38 +0300

    feat: add Yandex Metrika tracking

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Providers } from "./providers";
 
 const siteUrl = "https://radar.arvexo.ru";
@@ -47,7 +48,32 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ru">
-      <body><Providers>{children}</Providers></body>
+      <body>
+        <Script id="yandex-metrika" strategy="afterInteractive">
+          {`
+            (function(m,e,t,r,i,k,a){
+              m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+              m[i].l=1*new Date();
+              for (var j=0; j<document.scripts.length; j++) {
+                if (document.scripts[j].src===r) return;
+              }
+              k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a);
+            })(window,document,'script','https://mc.yandex.ru/metrika/tag.js?id=112824798','ym');
+            ym(112824798,'init',{ssr:true,webvisor:true,clickmap:true,ecommerce:'dataLayer',referrer:document.referrer,url:location.href,accurateTrackBounce:true,trackLinks:true});
+          `}
+        </Script>
+        <noscript>
+          <div>
+            {/* eslint-disable-next-line @next/next/no-img-element */}
+            <img
+              src="https://mc.yandex.ru/watch/112824798"
+              style={{ position: "absolute", left: "-9999px" }}
+              alt=""
+            />
+          </div>
+        </noscript>
+        <Providers>{children}</Providers>
+      </body>
     </html>
   );
 }
